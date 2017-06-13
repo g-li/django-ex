0534eea3b7fdcb71b2288fa177989886b3fb4df3
hzguoxunli <hzguoxunli@corp.netease.com>
2017-06-14 00:45:10 +0800

    sdf

--- ex1/app1/views.py
+++ ex1/app1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    # posts = Post.objects.filter(published_time__lte=timezone.now()).order_by('-publised_time')
+    posts = Post.objects.filter(title__contains='post').order_by('-published_time')
+    return render(request, 'app1/post_list.html', {'post':posts})
